aebcdbd1e9389f8a7e4a04ce0313e7a028827c10
YGM <739511296@qq.com>
2019-07-17 17:07:20 +0800

    轮播图组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+//es6编译(一定要写在最上边)
+import 'babel-polyfill'
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import  './common/styles/index.less'
 Vue.config.productionTip = false
+
+//解决移动端300毫秒延迟
+import fastclick from 'fastclick'
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
